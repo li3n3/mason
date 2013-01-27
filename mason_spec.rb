30b515f28ad448a72d9a8c2b84c4af1e3ce09834
Liene Verzemnieks <liene.v@gmail.com>
2013-01-27 12:51:28 -0800

    lots of notes and sketching out of ideas with Dan

--- mason_spec.rb
+++ mason_spec.rb
@@ -4,3 +4,25 @@ describe Mason do
 	it 'should print out latitude and longitude in hours/minutes/seconds'
 		p = Mason.new
 		p.
+
+
+# do I have an object that represents a photo?
+# do I have some data, and then it spits out lat/lon?
+
+# put this kinda stuff in photo_spec.rb
+# p = Photo.new("/tmp/photo.jpg")
+# p.latitude
+# => "0.22.33"
+# p = Photo.new("/tmp/photo.jpg")
+# p.parse_exif_data
+# => Exif(:latitude => 1, :long => 2)
+
+
+# write myself a Gemfile that has the dependencies I have in it (so I can require just the libraries I want)
+# Gemfile hides other gem dependencies from interfering (isolates dependencies)
+# put whichever libraries/gems I want in the Gemfile
+# "should be able to create a Photo with a path to it"
+# make a Fixtures directory -- put a photo with known attributes in there
+# Success: when you make a new photo and call latitude on it, and it works
+# Fixtures directory: stuff that's the same every time (fake data for testing)
+# write specs off a photo in the Fixtures file
