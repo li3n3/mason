5bbc622b815ef5814f74d53fa1697ae1a26dd9ea
Liene Verzemnieks <liene.v@gmail.com>
2013-01-27 12:50:52 -0800

    reads in an img path and prints out lat/long in fractional hours/minutes/seconds

--- readpicture.rb
+++ readpicture.rb
@@ -1,6 +1,16 @@
+require 'exifr'
+
 puts 'Path, please'
 photo_path       = gets.chomp
 # where it lives = that sans newline
 
 puts 'Thanks! Here\'s that file again.'
 puts photo_path
+
+print 'Here\'s the latitude: '
+
+puts EXIFR::JPEG.new(photo_path).gps_latitude.to_s
+
+print 'And here\'s the longitude: '
+
+puts EXIFR::JPEG.new(photo_path).gps_longitude.to_s
