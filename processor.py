4e1de56967c08e5c1ddd53725379eba29d186b7e
Liene Verzemnieks <liene.v@gmail.com>
2015-09-14 13:17:14 -0400

    little helper functions to pull out GPS tags + find things starting with substrings

--- processor.py
+++ processor.py
@@ -13,4 +13,22 @@ def grab_tags(filepath):
 
     return tags
 
+
+def starts_with(given_string, substring):
+    """ Returns boolean to indicate if the given_string starts with the substring. """
+    if substring == given_string[:len(substring)]:
+        return True
+    else:
+        return False
+
+
+def get_gps_tags(filepath = test_path):
+    """ Takes output from grab_tags, returning only the GPS tags. """
+    gps_tags = []
+    for tag in grab_tags(filepath).keys():
+        if starts_with(tag, 'GPS'):
+            gps_tags.append(tag)
+    return gps_tags
+
+
 print grab_tags(test_path)
